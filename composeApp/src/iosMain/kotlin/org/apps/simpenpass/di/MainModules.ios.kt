9af5866b04d347e0cc7799acb1311678a4293ed4
adamal26 <adam.alfarizi.2002@gmail.com>
2024-10-08 22:57:36 +0700

    fixing for ios datastore

--- composeApp/src/iosMain/kotlin/org/apps/simpenpass/di/MainModules.ios.kt
+++ composeApp/src/iosMain/kotlin/org/apps/simpenpass/di/MainModules.ios.kt
@@ -2,12 +2,26 @@ package org.apps.simpenpass.di
 
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.darwin.Darwin
+import kotlinx.cinterop.ExperimentalForeignApi
+import org.apps.simpenpass.utils.dataStoreFileName
 import org.koin.dsl.module
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
 
 actual fun platformModule() = module {
     single<HttpClientEngine> { Darwin.create() }
 }
 
+@OptIn(ExperimentalForeignApi::class)
 actual fun valuePathData(): String {
-    TODO("Not yet implemented")
+    val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null
+    )
+
+    return requireNotNull(documentDirectory).path + "/$dataStoreFileName"
 }
