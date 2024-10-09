23c3ee1e9d6f4971db26a290bb334ec54ae83939
Magang UDI 5 <magang5@psi.dinus.ac.id>
2024-10-09 13:15:56 +0700

    fixing for retrieve data from datastore

--- composeApp/src/iosMain/kotlin/org/apps/simpenpass/di/MainModules.ios.kt
+++ composeApp/src/iosMain/kotlin/org/apps/simpenpass/di/MainModules.ios.kt
@@ -2,6 +2,7 @@ package org.apps.simpenpass.di
 
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.darwin.Darwin
+import org.apps.simpenpass.utils.dataStoreFileName
 import org.koin.dsl.module
 
 actual fun platformModule() = module {
@@ -9,5 +10,13 @@ actual fun platformModule() = module {
 }
 
 actual fun valuePathData(): String {
-    TODO("Not yet implemented")
+    val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null
+    )
+
+    return requireNotNull(documentDirectory).path + "/$dataStoreFileName"
 }
