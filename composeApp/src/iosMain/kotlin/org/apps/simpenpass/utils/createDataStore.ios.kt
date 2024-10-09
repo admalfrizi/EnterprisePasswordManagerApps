50547a423a3db60fb625910dbdb089418526404a
adamal26 <adam.alfarizi.2002@gmail.com>
2024-10-09 14:15:08 +0700

    fixing for ios datastore

--- composeApp/src/iosMain/kotlin/org/apps/simpenpass/utils/createDataStore.ios.kt
+++ composeApp/src/iosMain/kotlin/org/apps/simpenpass/utils/createDataStore.ios.kt
@@ -2,7 +2,12 @@ package org.apps.simpenpass.utils
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
 
+@OptIn(ExperimentalForeignApi::class)
 actual fun createDataStore(context: Any?): DataStore<Preferences> {
     val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
         directory = NSDocumentDirectory,
