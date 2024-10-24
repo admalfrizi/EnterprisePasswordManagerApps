cd8aa1a12f620ac6e599c1c881acf865fac23076
Magang UDI 5 <magang5@psi.dinus.ac.id>
2024-10-24 19:36:13 +0700

    adding for search grup in dialog pop up 3

--- composeApp/src/commonMain/kotlin/org/apps/simpenpass/presentation/components/groupDtlComponents/GroupDtlLoadShimmer.kt
+++ composeApp/src/commonMain/kotlin/org/apps/simpenpass/presentation/components/groupDtlComponents/GroupDtlLoadShimmer.kt
@@ -1,6 +1,6 @@
 package org.apps.simpenpass.presentation.components.groupDtlComponents
 
-import androidx.compose.foundation.background
+ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
