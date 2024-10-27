f78a648b7c7bd1dae13efb03a2964fdb2a5ee38e
Adam Alfarizi <adam.alfarizi.2002@gmail.com>
2024-10-27 16:50:06 +0700

    change the url api address

--- composeApp/src/commonMain/kotlin/org/apps/simpenpass/utils/Constants.kt
+++ composeApp/src/commonMain/kotlin/org/apps/simpenpass/utils/Constants.kt
@@ -1,7 +1,7 @@
 package org.apps.simpenpass.utils
 
 object Constants {
-    const val HOST = "192.168.0.101"
-    const val BASE_API_URL = "http://$HOST/api-simpenpass/public/api/"
+    const val HOST = "192.168.18.29"
+    const val BASE_API_URL = "http://$HOST/api/"
     const val API_PATH = "api-simpenpass/public/api"
 }
