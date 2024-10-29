d9a45e6ebc21a4e4fc0a8ba991b6ff69d000be1d
adamalfarizi <adam.alfarizi.2002@gmail.com>
2024-10-29 14:18:27 +0700

    constants fix

--- composeApp/src/commonMain/kotlin/org/apps/simpenpass/utils/Constants.kt
+++ composeApp/src/commonMain/kotlin/org/apps/simpenpass/utils/Constants.kt
@@ -1,7 +1,8 @@
 package org.apps.simpenpass.utils
 
 object Constants {
-    const val HOST = "192.168.18.29"
-    const val BASE_API_URL = "http://$HOST/api/"
-    const val API_PATH = "api-simpenpass/public/api"
+    private const val HOST = "192.168.18.43"
+    private const val API_PATH = "simpenpass-api/public/api/"
+    const val BASE_API_URL = "http://$HOST/$API_PATH"
+
 }
