a82b14e8b06fc15246ed0fd8e9b180925a82a155
adamalfarizi <adam.alfarizi.2002@gmail.com>
2024-12-22 16:06:31 +0700

    patching for iosapp swift

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
@@ -8,3 +9,21 @@ struct iOSApp: App {
         }
     }
 }
+
+//@UIApplicationMain
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//    // Provide deepLinkFilter and deepLinkMapper if needed
+//    let rinku = RinkuIos.init(deepLinkFilter: nil, deepLinkMapper: nil)
+//    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+//        rinku.onDeepLinkReceived(url: url.absoluteString)
+//        return true
+//    }
+//
+//    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+//        if userActivity.activityType == NSUserActivityTypeBrowsingWeb, let url = userActivity.webpageURL {
+//            let urlString = url.absoluteString
+//            rinku.onDeepLinkReceived(userActivity: userActivity)
+//        }
+//        return true
+//    }
+//}
